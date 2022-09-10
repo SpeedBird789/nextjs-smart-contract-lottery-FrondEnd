7170dd3056827f4393b575448da9fc3f358ba51d
AN ❦ <108299225+krakxn@users.noreply.github.com>
2022-09-11 03:45:26 +0530

    fixes connectbutton

--- Components/Header.js
+++ Components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="border-b-2 flex-row">
       <h1 className="py-4 px-4 font-bold text-3xl">Decentralised Lottery</h1>
-      <ConnectButton moralisAuth={false}></ConnectButton>
+      <ConnectButton moralisAuth={false} />
     </div>
   );
 }
