6fb2adcbab25dd712e9af4fad6c86df9dd7cbd75
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-11 03:54:25 +0500

    Update Header.js

--- Components/Header.js
+++ Components/Header.js
@@ -2,9 +2,9 @@ import { ConnectButton } from "web3uikit";
 
 export default function Header() {
   return (
-    <div className="border-b-2 flex-row">
-      <h1 className="py-4 px-4 font-bold text-3xl">Decentralised Lottery</h1>
-      <ConnectButton moralisAuth={false} />
+    <div className="p-5 border-b-2 flex flex-row">
+      <h1 className="py-4 px-4 font-bold text-3xl">Decentralized Lottery</h1>
+      <div><ConnectButton moralisAuth={false} /></div>
     </div>
   );
 }
